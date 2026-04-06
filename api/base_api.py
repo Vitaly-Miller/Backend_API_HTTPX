3144680effab8e18bf4821525883cfb39880d762
Vitaly Miller <vital.miller@gmail.com>
2026-04-06 10:31:56 -0700

    ✔️

---
+++ api/base_api.py
@@ -0,0 +1,38 @@
+"""
+Base API
+(HTTPX)
+"""
+import httpx
+from data.data import Base
+
+#=======================================================================================================================
+class BaseAPI:
+    base_url = Base.URL
+
+    def request(self, method: str, endpoint: str, **kwargs) -> httpx.Response:
+        return httpx.request(
+            method=method,
+            url=f'{self.base_url}{endpoint}',
+            **kwargs
+        )
+
+    # 🟩GET
+    def get(self, endpoint: str, **kwargs) -> httpx.Response:
+        return self.request('GET', endpoint, **kwargs)
+
+    # 🟨POST
+    def post(self, endpoint: str, **kwargs) -> httpx.Response:
+        return self.request('POST', endpoint, **kwargs)
+
+    # 🟦PUT
+    def put(self, endpoint: str, **kwargs) -> httpx.Response:
+        return self.request('PUT', endpoint, **kwargs)
+
+    # 🟪PATCH
+    def patch(self, endpoint: str, **kwargs) -> httpx.Response:
+        return self.request('PATCH', endpoint, **kwargs)
+
+    # 🟥DELETE
+    def delete(self, endpoint: str, **kwargs) -> httpx.Response:
+        return self.request('DELETE', endpoint, **kwargs)
+#-----------------------------------------------------------------------------------------------------------------------

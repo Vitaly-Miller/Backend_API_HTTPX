90fb37c0fac28924a0d4d85725f926b7fdedaaf7
Vitaly Miller <vital.miller@gmail.com>
2026-04-08 20:04:18 -0700

    ✔️

--- HTTPX/requests VS. httpx.py
+++ HTTPX/requests VS. httpx.py
@@ -7,16 +7,17 @@ import requests
 import httpx
 
 #=======================================================================================================================
-#------------------------------------------------------ Regular --------------------------------------------------------
-# Parameters & data
+#------------------------------------------------ Parameters & Data ----------------------------------------------------
 cycles = 10                                          # Количество запросов
 url = 'https://jsonplaceholder.typicode.com/todos/1' # URL
 
-print('\n─────────────────────── Data ─────────────────────────')
+#---------------------------------------
+print('\n╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌ Data ╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌')
 print(f'URL: {url}')
 print(f'Количество запросов: {cycles}')
-print('\n────────────────────── Regular ───────────────────────')
-#---------------------------------------
+
+#------------------------------------------------------ Regular --------------------------------------------------------
+print('\n╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌ Regular ╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌')
 # requests:
 print('┌🔸requests:', end='')                        # Без перехода на новую строку
 time_start = time.time()
@@ -27,7 +28,7 @@ for _ in range(cycles):                              # Цикл (cycles раз)
 
 requests_time = round(time.time() - time_start, 3)
 avg_request_time = round(requests_time / cycles, 3)
-print(f'☑️{requests_time} sec. ({avg_request_time} sec./запрос)')
+print(f'☑️{requests_time:.3f} sec. ({avg_request_time:.3f} sec./запрос)')
 
 #---------------------------------------
 # httpx:
@@ -40,67 +41,65 @@ for _ in range(cycles):                              # Цикл (cycles раз)
 
 httpx_time = round(time.time() - time_start, 3)
 avg_httpx_time = round(httpx_time / cycles, 3)
-print(f'☑️{httpx_time} sec. ({avg_httpx_time} sec./запрос)')
+print(f'☑️{httpx_time:.3f} sec. ({avg_httpx_time:.3f} sec./запрос)')
 
 
-#---------------------------------------
-# РЕЗУЛЬТАТ (Regular)
+#---------------------
+# РЕЗУЛЬТАТ (Regular):
 timing_regular = [requests_time, httpx_time]
 percents_regular = f'{(round((max(timing_regular) / min(timing_regular) - 1) * 100)):g}%'
 print(f'Разница: {percents_regular}')
 
 
-
 #------------------------------------------------ Connection Pooling ---------------------------------------------------
-print('\n────────────────────── Connection Pooling ──────────────────────')
-#---------------------------------------
+print('\n╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌ Connection Pooling ╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌ ')
 # requests.Session()
-print('┌🔶requests.Session():', end='')             # Без перехода на новую строку
+print('┌🔶requests.Session():', end='')              # Без перехода на новую строку
 time_start = time.time()
 
-with requests.Session() as session:                 # 👈with (контекстный менеджер) - автоматически закрывает соединение
-    for _ in range(cycles):                         # Цикл (cycles раз)
-        response = session.get(url=url)             # 🟩GET-запрос (сессия)
-        print('.', end='')                          # Добавляет <.> предыдущей строке без переноса
+with requests.Session() as session:                  # 👈with (контекстный менеджер) - автоматически закрывает соединение
+    for _ in range(cycles):                          # Цикл (cycles раз)
+        response = session.get(url=url)              # 🟩GET-запрос (сессия)
+        print('.', end='')                           # Добавляет <.> предыдущей строке без переноса
 
 requests_session_time = round(time.time() - time_start, 3)
 avg_requests_session_time = round(requests_session_time / cycles, 3)
-print(f'☑️{requests_session_time} sec. ({avg_requests_session_time} sec./запрос)')
+print(f'☑️{requests_session_time:.3f} sec. ({avg_requests_session_time:.3f} sec./запрос)')
 
 #---------------------------------------
 # httpx.Client()
-print('└────🔷httpx.Client():', end='')            # Без перехода на новую строку
+print('└────🔷httpx.Client():', end='')              # Без перехода на новую строку
 time_start = time.time()
 
-with httpx.Client() as client:                      # 👈with (контекстный менеджер) - автоматически закрывает соединение
-    for _ in range(cycles):                         # Цикл (cycles раз)
-        response = client.get(url=url)              # 🟩GET-запрос (сессия)
-        print('.', end='')                          # Добавляет <.> предыдущей строке без переноса
+with httpx.Client() as client:                       # 👈with (контекстный менеджер) - автоматически закрывает соединение
+    for _ in range(cycles):                          # Цикл (cycles раз)
+        response = client.get(url=url)               # 🟩GET-запрос (сессия)
+        print('.', end='')                           # Добавляет <.> предыдущей строке без переноса
 
 httpx_client_time = round(time.time() - time_start, 3)
 avg_httpx_client_time = round(httpx_client_time / cycles, 3)
-print(f'☑️{httpx_client_time} sec. ({avg_httpx_client_time} sec./запрос)')
+print(f'☑️{httpx_client_time:.3f} sec. ({avg_httpx_client_time:.3f} sec./запрос)')
 
-#---------------------------------------
+#--------------------------------
 # РЕЗУЛЬТАТ (Connection Pooling):
 timing_session = [requests_session_time, httpx_client_time]
 percents_session = f'{(round((max(timing_session) / min(timing_session) - 1) * 100)):g}%'
 print(f'Разница: {percents_session}')
 
 
-#=======================================
-# ИТОГИ:
+#------------------------------------------------------- ИТОГИ: --------------------------------------------------------
 print(f"""\n
-╭─────────── requests ───────────╮
- ┌🔸requests         : {requests_time} sec.
- └🔶request.Session(): {requests_session_time} sec.
-╰────────────────────────────────╯
+╭╌╌╌╌╌╌╌╌╌╌╌ requests ╌╌╌╌╌╌╌╌╌╌╌╮
+ ┌🔸requests         : {requests_time:.3f} sec.
+ └🔶request.Session(): {requests_session_time:.3f} sec.
+╰╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╯
 Разница: {round((requests_time / requests_session_time - 1) * 100)}%
 
-╭──────────── httpx ─────────────╮
- ┌🔹httpx            : {httpx_time} sec.
- └🔷httpx.Client()   : {httpx_client_time} sec.
-╰────────────────────────────────╯
+╭╌╌╌╌╌╌╌╌╌╌╌╌ httpx ╌╌╌╌╌╌╌╌╌╌╌╌╌╮
+ ┌🔹httpx            : {httpx_time:.3f} sec.
+ └🔷httpx.Client()   : {httpx_client_time:.3f} sec.
+╰╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╯
 Разница: {round((httpx_time / httpx_client_time - 1) * 100)}%
 """)
 
+#-----------------------------------------------------------------------------------------------------------------------
